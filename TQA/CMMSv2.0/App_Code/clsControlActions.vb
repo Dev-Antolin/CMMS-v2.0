432021599447c3266059d7d1e97ca6086c9c7d13
Arthur <antolin.abad@mlhuillier1.com>
2013-11-13 10:39:23 -0800

    commit

---
+++ TQA/CMMSv2.0/App_Code/clsControlActions.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Web.HttpContext
+
+Public Class clsControlActions
+
+    Public Property Action() As String
+        Get
+            Return Current.Session("clickAction")
+        End Get
+        Set(ByVal value As String)
+            Current.Session("clickAction") = value
+        End Set
+    End Property
+
+End Class
